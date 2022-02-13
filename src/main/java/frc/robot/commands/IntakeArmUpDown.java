c0a39bf6ac933179fb6e14be3759e8bbd738e0d3
Cam Smith <csmith9358@gmail.com>
2022-02-12 21:43:27 -0600

    Remaped Survey

--- src/main/java/frc/robot/commands/IntakeArmUpDown.java
+++ src/main/java/frc/robot/commands/IntakeArmUpDown.java
@@ -54,7 +54,7 @@ public class IntakeArmUpDown extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        m_intakeArm.mymotorrun(0.6);
+        m_intakeArm.mymotorrun(0.2);
     }
 
     // Called once the command ends or is interrupted.

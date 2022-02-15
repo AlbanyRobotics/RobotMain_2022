d47c7105b4f0d734937a545ff55a6d8d541e659e
Cameron Smith <smith23@district745.org>
2022-02-15 17:44:25 -0600

    Increased Intake Speed

--- src/main/java/frc/robot/commands/IntakeSpinning.java
+++ src/main/java/frc/robot/commands/IntakeSpinning.java
@@ -55,7 +55,7 @@ public class IntakeSpinning extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        m_intakeSpin.mymotorrun(0.25);
+        m_intakeSpin.mymotorrun(0.30);
     }
 
     // Called once the command ends or is interrupted.

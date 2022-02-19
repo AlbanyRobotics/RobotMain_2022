33a9cb4433c59069754f1eca460c0d2dc832692d
Cameron Smith <smith23@district745.org>
2022-02-19 15:57:36 -0600

    Drive Distance

--- src/main/java/frc/robot/commands/Auto_basic_Shoot_Drive.java
+++ src/main/java/frc/robot/commands/Auto_basic_Shoot_Drive.java
@@ -7,6 +7,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.BallSurvey;
+import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ShootBall;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
@@ -14,15 +15,18 @@ import frc.robot.subsystems.ShootBall;
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class Auto_basic_Shoot_Drive extends SequentialCommandGroup {
   /** Creates a new Auto_basic_Shoot_Drive. */
-  public Auto_basic_Shoot_Drive(ShootBall shootBall, BallSurvey ballsurvey) {
+  public Auto_basic_Shoot_Drive(ShootBall shootBall, BallSurvey ballsurvey, DriveTrain driveTrain) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
                 parallel(new SpinFlywheels(() -> .18, () -> .61, shootBall).withTimeout(5),
                           sequence(new WaitCommand(2),
-                          new Survey(ballsurvey).withTimeout(5)
-                        )
-                ) 
+                                    new Survey(ballsurvey).withTimeout(5)
+                                  )
+                
+                        ), 
+                new Drive_For_Distance(12, () -> -0.5 , () -> 0.0, driveTrain),
+                new Drive_For_Distance(12, () -> 0.5 , () -> 0.0, driveTrain)
     );
   }
 }
--- src/main/java/frc/robot/commands/DriveWithJoysticks.java
+++ src/main/java/frc/robot/commands/DriveWithJoysticks.java
@@ -69,6 +69,7 @@ public class DriveWithJoysticks extends CommandBase {
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        m_driveTrain.my_drivercade(0,0);
     }
 
     // Returns true when the command should end.

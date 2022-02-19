3573b8f89b980c98cce1d097f4a5272d7636618f
Cameron Smith <smith23@district745.org>
2022-02-19 11:52:58 -0600

    LimeLight and Turret Con Vel Controler

--- src/main/java/frc/robot/commands/RotateLeft.java
+++ src/main/java/frc/robot/commands/RotateLeft.java
@@ -54,14 +54,16 @@ public class RotateLeft extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        m_turretCon.mymotorrun(0.1);
+        //m_turretCon.mymotorrun(0.1);
+        m_turretCon.mymotorVelocity(-50);
 
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-        m_turretCon.mymotorrun(0);
+        //m_turretCon.mymotorrun(0);
+        m_turretCon.mymotorVelocity(0);
     }
 
     // Returns true when the command should end.
